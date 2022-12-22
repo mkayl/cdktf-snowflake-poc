388e7b8bb15be40988d8210426c42d724f8a08be
Michael Wyss <michael.wyss@homegate.ch>
2022-12-22 16:19:53 +0100

    Create a database and schema

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from constructs import Construct
 from cdktf import App, TerraformStack, CloudBackend, NamedCloudWorkspace, TerraformVariable
 from cdktf_cdktf_provider_snowflake.provider import SnowflakeProvider
+from cdktf_cdktf_provider_snowflake.database import Database
+from cdktf_cdktf_provider_snowflake.schema import Schema
 
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, id: str):
@@ -18,6 +20,8 @@ class MyStack(TerraformStack):
                           username=snowflake_username.string_value,
                           password=snowflake_password.string_value)
 
+        test_database = Database(self, id_="test_database", name='TEST_DATABASE')
+        test_schema = Schema(self, id_='test_schema', database=test_database.name, name='TEST_SCHEMA')
 
 app = App()
 stack = MyStack(app, "cdktf-snowflake-poc")
