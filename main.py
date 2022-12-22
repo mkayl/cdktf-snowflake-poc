2b71052715eadf829b35bd846313ae0c1c4ba52b
Michael Wyss <michael.wyss@homegate.ch>
2022-12-22 16:18:48 +0100

    Configure Snowflake provider and secure authentication mechanism using TerraformVariable and remote operations

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 from constructs import Construct
-from cdktf import App, TerraformStack, CloudBackend, NamedCloudWorkspace
-
+from cdktf import App, TerraformStack, CloudBackend, NamedCloudWorkspace, TerraformVariable
+from cdktf_cdktf_provider_snowflake.provider import SnowflakeProvider
 
 class MyStack(TerraformStack):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
 
-        # define resources here
+        snowflake_account = TerraformVariable(self, "snowflake_account", type="string")
+
+        snowflake_username = TerraformVariable(self, "snowflake_username", type="string")
+
+        snowflake_password = TerraformVariable(self, "snowflake_password", type="string", sensitive=True)
+
+        SnowflakeProvider(self, 'snowflake',
+                          account=snowflake_account.string_value,
+                          username=snowflake_username.string_value,
+                          password=snowflake_password.string_value)
 
 
 app = App()
